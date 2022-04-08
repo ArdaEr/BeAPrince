94fba0938737eab5a1535578bd0ad0b85c912089
ArdaEr <cardaerdogan@hotmail.com>
2022-04-08 22:52:03 +0300

    Fixed Bugs

---
+++ Assets/Scripts/EndLevelScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+
+public class EndLevelScript : MonoBehaviour
+{
+    [SerializeField] float LevelLoadDelay = 0.5F;
+    [SerializeField] PlayableDirector isEnoughCoin;
+    Animator _anim;
+    private void Start() {
+        _anim = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            if(FindObjectOfType<GameSession>().score >= 15 )
+            {
+                _anim.SetBool("Open", true);
+                StartCoroutine(LoadNextLevel());
+            }
+            else
+            {
+                isEnoughCoin.Play(); 
+            }
+        }
+        
+    }
+    IEnumerator LoadNextLevel()
+    {  
+        
+        yield return new WaitForSecondsRealtime(LevelLoadDelay);
+        FindObjectOfType<ScenePersist>().ScenePersistDeath();
+        SceneManager.LoadScene("WakeUpScene");
+        
+    }
+}
